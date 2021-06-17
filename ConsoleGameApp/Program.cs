8e4eb25cfac2c572bd8cf95d7fee23b213fbe000
StefanBrunotte <stefan.brunotte@prog-it.se>
2021-06-17 10:02:10 +0200

    Add project files.

---
+++ ConsoleGameApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGameApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
